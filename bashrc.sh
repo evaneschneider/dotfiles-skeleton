7736cb0a78f01bc2401fe088b73b33668d9b5078
Evan Schneider <eschneider@pitt.edu>
2020-10-30 15:29:09 -0400

    Fixed hostname

--- bashrc.sh
+++ bashrc.sh
@@ -15,11 +15,11 @@ bash_sources=(
 # a directory matching the host's name
 declare -A host_dirs
     # Personal Machines
+host_dirs["Evans-MacBook-Pro-2.local"]="Evans-MBP-2"
     
     # Remote Machines
 host_dirs["login0.crc.pitt.edu"]="crc_h2p"
 host_dirs["login1.crc.pitt.edu"]="crc_h2p"
-host_dirs["Evans-MBP-2"]="Evans-MBP-2"
 
 # Source bash files at the root of the dotfile repository and from host
 # repo_directories from the root of the dotfile repository (eg. ${repo}/host1/bashrc.sh)
